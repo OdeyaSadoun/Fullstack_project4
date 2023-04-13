5d3a883cdeba6d79fffee884ffa33916eb0ef7ba
אודיה סעדון <osadoun@g.jct.ac.il>
2023-04-13 18:56:02 +0300

    add ChangeLang to text editor

--- src/Components/textEditor.jsx
+++ src/Components/textEditor.jsx
@@ -4,12 +4,14 @@ import Size from './sizes';
 import TextArea from './textArea';
 import ColorPalette from './colorPalette';
 import SpecialSetting from './specialSetting';
+import ChangeLang from './changeLang';
 
 class TextEditor extends Component {
   render() {
     return (
       <div className="keyboard-container">
         <TextArea />
+        <ChangeLang/>
         <Keyboard />
         <Size />
         <ColorPalette />
