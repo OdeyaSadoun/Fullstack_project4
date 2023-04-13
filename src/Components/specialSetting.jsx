10b78fc82bfe335497bd83dd6c933fdf30c2860b
אודיה סעדון <osadoun@g.jct.ac.il>
2023-04-13 22:08:13 +0300

    More

--- src/Components/specialSetting.jsx
+++ src/Components/specialSetting.jsx
@@ -1,17 +1,54 @@
 import React, { Component } from 'react';
+import '../CSS/specialSetting.css';
+
 class SpecialSetting extends Component {
-    state = {  } 
-    render() { 
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: ''        };
+    }
+
+    handleKeyPress = key => {
+        if (key === 'Clear all') {
+            this.setState({ text: '' });
+        } else if (key === 'Upper all') {
+            this.setState(prevState => ({
+                text: prevState.text.toUpperCase()
+            }));
+        } else if (key === 'Lower all') {
+            this.setState(prevState => ({
+                text: prevState.text.toLowerCase()
+            }));
+        }
+    };
+
+    render() {
+        let options = [
+            { label: 'Clear all', value: 'Clear all' },
+            { label: 'Upper all', value: 'Upper all' },
+            { label: 'Lower all', value: 'Lower all' }
+        ];
         return (
-            <div>
-                <button>Clear all</button>
-                <button>Upper all</button>
-                <button>Lower all</button>
-                <button>Ctrl Z</button>
-                <button>Ctrl Y</button>
+            <div className="options-container">
+                {options.map((key, index) => {
+                    let classes = 'option specials';
+                    if (key.value === 'Clear all') classes += 'option-clear-all';
+                    if (key.value === 'Upper all') classes += 'option-upper-all';
+                    if (key.value === 'Lower all') classes += 'option-lower-all';
+
+                    return (
+                        <button
+                            key={index}
+                            className={classes}
+                            onClick={() => this.handleKeyPress(key.value)}
+                        >
+                            {key.value}
+                        </button>
+                    );
+                })}
             </div>
         );
     }
 }
- 
+
 export default SpecialSetting;
