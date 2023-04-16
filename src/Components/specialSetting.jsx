cd9cf720945d8de684f7e74fb583c02e97ed1fdd
אודיה סעדון <osadoun@g.jct.ac.il>
2023-04-16 17:53:20 +0300

    delete lines not neccery

--- src/Components/specialSetting.jsx
+++ src/Components/specialSetting.jsx
@@ -31,33 +31,12 @@ class SpecialSetting extends Component {
 
 
     render() {
-        // let options = [
-        //     { label: 'Clear all', value: 'Clear all' },
-        //     { label: 'Upper all', value: 'Upper all' },
-        //     { label: 'Lower all', value: 'Lower all' }
-        // ];
+
         return (
             <div className="options-container option specials">
                 <button className='option-clear-all' value={'Clear all'} onClick={() => this.handleClick('Clear all')}>Clear all</button>
                 <button className='option-upper-all' value={'Upper all'} onClick={() => this.handleClick('Upper all')}>Upper all</button>
                 <button className='option-lower-all' value={'Lower all'} onClick={() => this.handleClick('Lower all')}>Lower all</button>
-                {/* 
-                {options.map((key, index) => {
-                    let classes = 'option specials';
-                    if (key.value === 'Clear all') classes += 'option-clear-all';
-                    if (key.value === 'Upper all') classes += 'option-upper-all';
-                    if (key.value === 'Lower all') classes += 'option-lower-all';
-
-                    return (
-                        <button
-                            key={index}
-                            className={classes}
-                            onClick={() => this.handleButtonSpecialSettingClick(key.value)}
-                        >
-                            {key.value}
-                        </button>
-                    );
-                })} */}
             </div>
         );
     }
