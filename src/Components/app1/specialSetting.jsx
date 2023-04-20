a59a05cb45ab7e6ac316afa797abd4443bc1d2d0
אודיה סעדון <osadoun@g.jct.ac.il>
2023-04-20 10:10:17 +0300

    uper all and lower all change with map func

--- src/Components/app1/specialSetting.jsx
+++ src/Components/app1/specialSetting.jsx
@@ -14,18 +14,26 @@ class SpecialSetting extends Component {
     handleClick = key => {
         let { text, language } = this.props; //for real time updates
         console.log('text', text);
-        if (language === 'english') {
-            if (key === 'Clear all') {
-                text = '';
-            } else if (key === 'Upper all') {
-                console.log('upper', text);
-                text = text.toUpperCase();
-            } else if (key === 'Lower all') {
-                console.log('lower', text);
-                text = text.toLowerCase();
-            }
+
+        if (key === 'Upper all') {
+            console.log('upper', text);
+            text = text.map(str => str.toUpperCase());
+            this.props.onClick(text);
+
+        } else if (key === 'Lower all') {
+            console.log('lower', text);
+            text = text.map(str => str.toLowerCase());
+            this.props.onClick(text);
         }
-        this.props.onClick(text);
+
+        if (key === 'Clear all') {
+            text = '';
+        }
+        else if (key === 'All') {
+            this.props.onAllButton()();
+        } else if (key === 'Single') {
+            this.props.onSingleButton()();
+        }
     };
 
 
@@ -34,6 +42,8 @@ class SpecialSetting extends Component {
 
         return (
             <div className="options-container option specials">
+                <button className='option-all' value={'All'} onClick={() => this.handleClick('All')}>All</button>
+                <button className='option-single' value={'Single'} onClick={() => this.handleClick('Single')}>Single</button>
                 <button className='option-clear-all' value={'Clear all'} onClick={() => this.handleClick('Clear all')}>Clear all</button>
                 <button className='option-upper-all' value={'Upper all'} onClick={() => this.handleClick('Upper all')}>Upper all</button>
                 <button className='option-lower-all' value={'Lower all'} onClick={() => this.handleClick('Lower all')}>Lower all</button>
